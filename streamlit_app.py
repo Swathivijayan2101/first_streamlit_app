09e4a9c93d9aff39a629ecdab17958efd8124296
Swathivijayan2101 <133234489+Swathivijayan2101@users.noreply.github.com>
2023-05-11 19:31:18 +0530

    Update streamlit_app.py
    Added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit
 
 streamlit.title('My Parents New Helathy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('French Toast - Milk')
+streamlit.text('Vegetable Salad - Boiled Egg - Milk')
+streamlit.text('Sandwich - Spinach - Milk')
+
+streamlit.header('Lunch Menu')
+streamlit.text('Chicken Briyani')
+streamlit.text('Rice - Dhal')
+streamlit.text('Mutton Briyani')
